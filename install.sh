548d878ac990c4e6f3474ed3286f37f1a1ad747a
pep3309531 <pep3309531@163.com>
2016-08-08 10:55:53 +0800

    修改：     README.md 	新文件：   install.sh 	重命名：   config -> trunk/config 	新文件：   trunk/configs/boards/XZB/board.h 	新文件：   trunk/configs/boards/XZB/board.mk 	新文件：   trunk/configs/boards/XZB/kernel-3.4.x.config 	新文件：   trunk/configs/boards/XZB/libc.config 	新文件：   trunk/configs/boards/uclibc-mipsel.config 	新文件：   trunk/user/shared/defaults.h 	新文件：   trunk/user/www/Makefile 	新文件：   trunk/user/www/dict/CN.dict 	新文件：   trunk/user/www/n56u_ribbon_fixed/device-map/clients.asp

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# DESTDIR is your target project's dir
+DESTDIR=/opt/rt-n56u
+
+ROOTDIR=`pwd`
+
+if [ ! -d "$DESTDIR" ] ; then
+	echo "Target project directory not exists! Terminate."
+	exit 1
+fi
+
+echo "-------------COPY-FILES---------------"
+
+if [ -d "$ROOTDIR/trunk/user/" ] ; then
+	cp -fRv "$ROOTDIR/trunk/user/" "$DESTDIR/trunk/"
+fi
+
+echo "-------------COPY-END---------------"
---
+++ trunk/configs/boards/XZB/board.h
@@ -0,0 +1,35 @@
+/* Xunlei XZB (TimeCloud) */
+
+#define BOARD_PID		"XZB"
+#define BOARD_NAME		"XZB (TimeCloud)"
+#define BOARD_DESC		"Xunlei XZB (TimeCloud)"
+#define BOARD_VENDOR_NAME	"Xunlei(Shenzheng) Limited"
+#define BOARD_VENDOR_URL	"http://www.xunlei.com/"
+#define BOARD_MODEL_URL		"http://xzb.xunlei.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	13
+#define BOARD_GPIO_BTN_WPS	4
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef BOARD_GPIO_LED_SW2G		/* soft led */
+#undef BOARD_GPIO_LED_SW5G		/* soft led */
+#undef BOARD_GPIO_LED_POWER
+#undef BOARD_GPIO_LED_LAN
+#undef BOARD_GPIO_LED_WAN
+#define	 BOARD_GPIO_LED_USB	8
+#define  BOARD_GPIO_LED_ROUTER	7
+#define  BOARD_GPIO_PWR_USB	11
+#undef  BOARD_GPIO_PWR_USB2	//9
+#define  BOARD_GPIO_PWR_USB_ON	1
+#undef BOARD_HAS_5G_11AC	//1
+#undef BOARD_NUM_ANT_5G_TX	//2
+#undef BOARD_NUM_ANT_5G_RX	//2
+#undef BOARD_NUM_ANT_2G_TX	//2
+#undef BOARD_NUM_ANT_2G_RX	//2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
+
